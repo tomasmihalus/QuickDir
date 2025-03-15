e0abaa47e23bccae21bf1621c33a6a54e2714e73
tomasmihalus <tomas.mihalus@gmail.com>
2025-03-15 07:59:56 +0100

    #fixed missing project files, great start

---
+++ QuickDir/Program.cs
@@ -0,0 +1,36 @@
+﻿using QuickDir.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickDir {
+    internal static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+
+            List<MenuItem> items = new List<MenuItem>();
+
+            if (File.Exists("user.cfg")) {
+                foreach (string line in File.ReadLines("user.cfg")) {
+
+                }
+            }
+
+            using (NotifyIcon icon = new NotifyIcon()) {
+                icon.Icon = QuickResources.tray_icon;
+                icon.ContextMenu = new ContextMenu();
+            }
+
+            Application.Exit();
+        }
+    }
+}
